f30790633e2caa13bd324de0f3f6d86ba8c8ddc0
Joe <yu.xue.work@outlook.com>
2023-07-29 15:54:30 +0800

    feat: add struct representing a command.

--- src/kv.rs
+++ src/kv.rs
@@ -1,4 +1,16 @@
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
+use std::fs::{self, File, OpenOptions};
+use std::io::{self, BufReader, BufWriter, Read, Seek, SeekFrom, Write};
+use std::ops::Range;
+use std::path::{Path, PathBuf};
+
+use serde::{Deserialize, Serialize};
+use serde_json::Deserializer;
+
+use crate::{KvsError, Result};
+use std::ffi::OsStr;
+
+const COMPACTION_THRESHOLD: u64 = 1024 * 1024;
 
 #[derive(Default)]
 /// KvStore basic
@@ -10,9 +22,7 @@ pub struct KvStore {
 impl KvStore {
     /// 创建一个 'KvStore {map}'
     pub fn new() -> KvStore {
-        KvStore {
-            map: HashMap::new(),
-        }
+        KvStore { map: HashMap::new() }
     }
 
     /// 向 KvStore map 存入新的 key - value
@@ -28,5 +38,22 @@ impl KvStore {
     /// 根据 key 从 KvStore 中删除相应的 key - value
     pub fn remove(&mut self, key: String) {
         self.map.remove(&key);
+    }
+}
+
+/// 代表命令的 struct
+#[derive(Serialize, Deserialize, Debug)]
+enum Command {
+    Set { key: String, value: String },
+    Remove { key: String },
+}
+
+impl Command {
+    fn set(key: String, value: String) -> Command {
+        Command::Set { key, value }
+    }
+
+    fn remove(key: String) -> Command {
+        Command::Remove { key }
     }
 }
