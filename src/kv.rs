f3ee83f8e502a82889acff5c2d3fc86496519133
Joe <yu.xue.work@outlook.com>
2023-07-29 16:11:25 +0800

    feat: add BufReaderWithPos

--- src/kv.rs
+++ src/kv.rs
@@ -74,3 +74,33 @@ impl From<(u64, Range<u64>)> for CommandPos {
         }
     }
 }
+
+struct BufReaderWithPos<R: Read + Seek> {
+    reader: BufReader<R>,
+    pos: u64,
+}
+
+impl<R: Read + Seek> BufReaderWithPos<R> {
+    fn new(mut inner: R) -> Result<Self> {
+        let pos = inner.seek(SeekFrom::Current(0))?;
+        Ok(BufReaderWithPos { 
+            reader: BufReader::new(inner), 
+            pos,
+        })
+    }
+}
+
+impl<R: Read + Seek> Read for BufReaderWithPos<R> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        let len = self.reader.read(buf)?;
+        self.pos += len as u64;
+        Ok(len)
+    }
+}
+
+impl<R: Read + Seek> Seek for BufReaderWithPos<R> {
+    fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> {
+        self.pos = self.reader.seek(pos)?;
+        Ok(self.pos)
+    }
+}
