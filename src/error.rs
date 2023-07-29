8847ff1f82d8767204068475df8a14f5aa9f9142
Joe <yu.xue.work@outlook.com>
2023-07-29 15:07:00 +0800

    feat: complete error

--- src/error.rs
+++ src/error.rs
@@ -18,3 +18,18 @@ pub enum KvsError {
     #[fail(display = "Unexpected command type")]
     UnexpectedCommandType,
 }
+
+impl From<io::Error> for KvsError {
+    fn from(err: io::Error) -> KvsError {
+        KvsError::Io(err)
+    }
+}
+
+impl From<serde_json::Error> for KvsError {
+    fn from(err: serde_json::Error) -> KvsError {
+        KvsError::Serde(err)
+    }
+}
+
+/// Result type for kvs
+pub type Result<T> = std::result::Result<T, KvsError>;
