2b2b8edf4b1c1a9c415d9f24d38c2899a5d5dff3
Joe <yu.xue.work@outlook.com>
2023-08-12 15:01:04 +0800

    feat: add cilent

---
+++ src/client.rs
@@ -0,0 +1,35 @@
+use crate::common::{GetResponse, RemoveResponse, Request, SetResponse};
+use crate::{KvsError, Result};
+use serde::Deserialize;
+use serde_json::de::{Deserializer, IoRead};
+use std::io::{BufReader, BufWriter, Write};
+use std::net::{TcpStream, ToSocketAddrs};
+
+/// key-value store 客户端.
+pub struct KvsClient {
+    reader: Deserializer<IoRead<BufReader<TcpStream>>>,
+    writer: BufWriter<TcpStream>,
+}
+
+impl KvsClient {
+    /// 连接到 addr 以访问 KvsServer
+    pub fn connect<A: ToSocketAddrs>(addr: A) -> Result<Self> {
+        let tcp_reader = TcpStream::connect(addr);
+        let tcp_writer = tcp_reader.try_clone()?;
+        Ok(KvsClient {
+            reader: Deserializer::from_reader(BufReader::new(tcp_reader)),
+            writer: BufWriter::new(tcp_writer),
+        })
+    }
+
+    /// 从服务器获取给定键的值
+    pub fn get(&mut self, key: String) -> Result<Option<String>> {
+        serde_json::to_writer(&mut self.writer, &Request::Get { key })?;
+        self.writer.flush()?;
+        let resp = GetResponse::deserialize(&mut self.reader)?;
+        match resp {
+            GetResponse::Ok(value) => Ok(value),
+            GetResponse::Err(msg) => Err(KvsError::StringError(msg)),
+        }
+    }
+}
