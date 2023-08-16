9996915d66c09aedb17a130dc199322cb008bbd7
Joe <yu.xue.work@outlook.com>
2023-08-16 14:26:52 +0800

    feat: update kvs client error

--- src/bin/kvs-client.rs
+++ src/bin/kvs-client.rs
@@ -11,9 +11,8 @@ const ADDRESS_FORMAT: &str = "IP:PORT";
 #[structopt(
     name = "kvs-client",
     raw(global_settings = "&[\
-                            AppSettings::DisableHelpSubcommand,\
-                            AppSettings::VersionlessSubcommands\
-                            ]")
+                           AppSettings::DisableHelpSubcommand,\
+                           AppSettings::VersionlessSubcommands]")
 )]
 struct Opt {
     #[structopt(subcommand)]
@@ -30,7 +29,7 @@ enum Command {
             long,
             help = "Sets the server address",
             raw(value_name = "ADDRESS_FORMAT"),
-            raw(default_value = "DEFAULT_LISTENING_ADDRESS")
+            raw(default_value = "DEFAULT_LISTENING_ADDRESS"),
             parse(try_from_str)
         )]
         addr: SocketAddr,
@@ -45,11 +44,12 @@ enum Command {
             long,
             help = "Sets the server address",
             raw(value_name = "ADDRESS_FORMAT"),
-            raw(default_value = "DEFAULT_LISTENING_ADDRESS")
+            raw(default_value = "DEFAULT_LISTENING_ADDRESS"),
             parse(try_from_str)
         )]
         addr: SocketAddr,
     },
+    #[structopt(name = "rm", about = "Remove a given string key")]
     Remove {
         #[structopt(name = "KEY", help = "A string key")]
         key: String,
@@ -57,7 +57,7 @@ enum Command {
             long,
             help = "Sets the server address",
             raw(value_name = "ADDRESS_FORMAT"),
-            raw(default_value = "DEFAULT_LISTENING_ADDRESS")
+            raw(default_value = "DEFAULT_LISTENING_ADDRESS"),
             parse(try_from_str)
         )]
         addr: SocketAddr,
@@ -65,19 +65,18 @@ enum Command {
 }
 
 fn main() {
-   let opt = Opt::from_args();
-   if let Err(e)  = run(opt) {
-       eprintln!("Error: {}", e);
-       exit(1);
-   }
+    let opt = Opt::from_args();
+    if let Err(e) = run(opt) {
+        eprintln!("{}", e);
+        exit(1);
+    }
 }
 
 fn run(opt: Opt) -> Result<()> {
     match opt.command {
         Command::Get { key, addr } => {
             let mut client = KvsClient::connect(addr)?;
-            let value = client.get(key)?;
-            if let Some(value) = value {
+            if let Some(value) = client.get(key)? {
                 println!("{}", value);
             } else {
                 println!("Key not found");
