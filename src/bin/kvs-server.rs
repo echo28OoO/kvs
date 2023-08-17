029e6b9e245c9921a7fd8b4deaf5a615e12ee1d7
Joe <yu.xue.work@outlook.com>
2023-08-17 14:30:08 +0800

    feat: update kvs server

---
+++ src/bin/kvs-server.rs
@@ -0,0 +1,61 @@
+use clap::arg_enum;
+use kvs::*;
+use log::LevelFilter;
+use log::{error, info, warn};
+use std::env::current_dir;
+use std::fs;
+use std::net::SocketAddr;
+use std::process::exit;
+use structopt::StructOpt;
+
+const DEFAULT_LISTENING_ADDRESS: &str = "127.0.0.1:4000";
+const DEFAULT_ENGINE: Engine = Engine::kvs;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "kvs-server")]
+struct Opt {
+    #[structopt(
+        long,
+        help = "Sets the listening address",
+        value_name = "IP:PORT",
+        raw(default_value = "DEFAULT_LISTENING_ADDRESS"),
+        parse(try_from_str)
+    )]
+    addr: SocketAddr,
+    #[structopt(
+        long,
+        help = "Sets the storage engine",
+        value_name = "ENGINE-NAME",
+        raw(possible_values = "&Engine::variants()")
+    )]
+    engine: Option<Engine>,
+}
+
+arg_enum! {
+    #[allow(non_camel_case_types)]
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    enum Engine {
+        kvs,
+        sled,
+    }
+}
+
+fn run_with_engine<E: KvsEngine>(engine: E, addr: SocketAddr) -> Result<()> {
+    let server = KvsServer::new(engine);
+    server.run(addr)
+}
+
+fn current_engine() -> Result<Option<Engine>> {
+    let engine = current_dir()?.join("engine");
+    if !engine.exists() {
+        return Ok(None);
+    }
+
+    match fs::read_to_string(engine)?.parse() {
+        Ok(engine) => Ok(Some(engine)),
+        Err(e) => {
+            warn!("The content of engine file is invalid: {}", e);
+            Ok(None)
+        }
+    }
+}
