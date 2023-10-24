ba216b3019ecb0eb2f8f890cdeff740e066b8530
Joe <yu.xue.work@outlook.com>
2023-10-24 16:13:49 +0800

    feat: naive compeleted

---
+++ src/thread_pool/naive.rs
@@ -0,0 +1,21 @@
+use std::thread;
+
+use super::ThreadPool;
+use crate::Result;
+
+/// 它实际上不是一个线程池。每次调用`spawn`方法时，它会生成一个新线程
+
+pub struct NaiveThreadPool;
+
+impl ThreadPool for NaiveThreadPool {
+    fn new(_threads: u32) -> Result<Self> {
+        Ok(NaiveThreadPool)
+    }
+
+    fn spawn<F>(&self, job: F)
+    where 
+        F: FnOnce() + Send + 'static,
+    {
+        thread::spawn(job);
+    }
+ }
