1cf0c59497fa64d38493c266bd9e9fa376c6f51d
Joe <yu.xue.work@outlook.com>
2023-10-26 17:22:52 +0800

    feat: add shared queue for TaskReceiver

--- src/thread_pool/shared_queue.rs
+++ src/thread_pool/shared_queue.rs
@@ -29,3 +29,27 @@ impl ThreadPool for SharedQueueThreadPool {
     }
 }
 
+#[derive(Clone)]
+struct TaskReceiver(Receiver<Box<dyn FnOnce() + Send + 'static>>);
+
+impl Drop for TaskReceiver {
+    fn drop(&mut self) {
+        if thread::panicking() {
+            let rx = self.clone();
+            if let Err(e) = thread::Builder::new().spawn(move || run_tasks(rx)) {
+                error!("Failed to spawn a thread: {}", e);
+            }
+        }
+    }
+}
+
+fn run_tasks(rx: TaskReceiver) {
+    loop {
+        match rx.0.recv() {
+            Ok(task) => {
+                task();
+            }
+            Err(_) => debug!("Thread exits because th thread pool is destroyed."),
+        }
+    }
+}
