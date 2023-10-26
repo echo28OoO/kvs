6e46e1ae22876b1e61f8019180f03f83d5ada311
Joe <yu.xue.work@outlook.com>
2023-10-26 17:18:55 +0800

    feat: add struct shared queue thread pool

---
+++ src/thread_pool/shared_queue.rs
@@ -0,0 +1,31 @@
+use std::thread;
+
+use super::ThreadPool;
+use crate::Result;
+
+use crossbeam::channel::{self, Receiver, Sender};
+
+use log::{debug, error};
+
+/// 注意事项：线程池不是使用 `catch_unwind` 实现的，因为
+/// 它要求任务是 `UnwindSafe`。
+
+/// 内部使用共享队列的线程池。
+/// 
+/// 如果生成的任务发生 panics，旧线程将被销毁，并创建一个新线程。
+/// 当创建线程池后捕获到在操作系统级别创建线程的任何失败时，
+/// 它的失败会线程池创建后被捕获。所以池中的线程数可以减少到零，
+/// 然后讲任务派生到线程池会出现 panics
+pub struct SharedQueueThreadPool {
+    tx: Sender<Box<dyn FnOnce() + Send + 'static>>,
+}
+
+impl ThreadPool for SharedQueueThreadPool {
+    fn new (threads: u32) -> Result<Self> {
+        let (tx, rx) = channel::unbounded::<Box<dyn FnOnce() + Send + 'static>>();
+        for _ in 0..threads {
+            ..
+        }
+    }
+}
+
